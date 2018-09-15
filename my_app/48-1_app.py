c87c9d6fd66d301dace8ab2bf3d40197deb441c8
Austin Krauss <austin.f.krauss@gmail.com>
2018-09-15 15:57:11 -0700

    upload website design changes

--- my_app/48-1_app.py
+++ my_app/48-1_app.py
@@ -15,22 +15,7 @@ def welcome_page():
     return render_template('index.html')
 
 
-@app.route('/category/<category_name>', methods=['GET'])
-def load_category(category_name):
-    data = pickle.load( open( "../web_data/ML_data.pkl", "rb" ) )
-    data['url_cat'] = data['category'].apply(urllib.parse.quote)
-    pages = list(zip(data['category'], data['Predicted_Quality']))
-    return render_template('category.html', data=data, pages=pages, category_name=category_name)
-
-@app.route('/sub_category/<sub_category_name>', methods=['GET'])
-def load_sub_category(sub_category_name):
-    data = pickle.load( open( "../web_data/ML_pages.pkl", "rb" ) )
-    data = data[data['category'] == sub_category_name]
-    data['url_cat'] = data['page'].apply(urllib.parse.quote)
-    return render_template('sub_category.html', data=data, sub_category_name=sub_category_name)
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=False)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
 
