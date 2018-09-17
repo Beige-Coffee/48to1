16d0729f7e3b3617a8ec8b116137e0f57b37782d
Austin Krauss <austin.f.krauss@gmail.com>
2018-09-16 17:00:30 -0700

    upload progress on submission page for website

--- my_app/48-1_app.py
+++ my_app/48-1_app.py
@@ -14,6 +14,9 @@ app = Flask(__name__,
 def welcome_page():
     return render_template('index.html')
 
+@app.route('/get_started', methods=['GET'])
+def load_get_started():
+    return render_template('get_started.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
